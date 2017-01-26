6b60303334c7e8d28c055331261a25446e460995
DamonToumbourou <dtoumbourou@gmail.com>
2017-01-26 20:58:33 +1100

    finished TD scrapers

--- scrapers.py
+++ scrapers.py
@@ -329,14 +329,290 @@ class WebScrapers(object):
         content = soup.find('div', {'class': 'table-responsive rate-table-rhs'})
         rates = content.find_all('tr')
 
+        ubank_td = []
+        standard = []
+        loyalty = []
         for rate in rates:
             found = rate.find('td')
             if found is not None:
                 if 'UBank - Standard rate' in found.string: 
-                    stand = found.find_all('
-                    print rate
+                    stand = rate.find_all('td')
+                    
+                    short = { 
+                        'months': '3',
+                        'rate': stand[1].string
+                    }
+                    standard.append(short)
+                    
+                    mid = { 
+                        'months': '6',
+                        'rate': stand[3].string
+                    }
+                    standard.append(mid)
+                    
+                    long_ = { 
+                        'months': '12',
+                        'rate': stand[5].string
+                    }
+                    standard.append(long_)
+                    
+                if 'UBank - with Loyalty Bonus' in found.string: 
+                    stand = rate.find_all('td')
+                    
+                    short = { 
+                        'months': '3',
+                        'rate': stand[1].string
+                    }
+                    loyalty.append(short)
+                    
+                    mid = { 
+                        'months': '6',
+                        'rate': stand[3].string
+                    }
+                    loyalty.append(mid)
+                    
+                    long_ = { 
+                        'months': '12',
+                        'rate': stand[5].string
+                    }
+                    loyalty.append(long_)
 
-        return None
+        ubank_td.append([{
+            'standard': standard, 
+            'loyalty': loyalty
+        }])
+
+        return ubank_td
+    
+
+    def get_boq_td(self):
+        url = 'http://www.boq.com.au/todays_rates_term_deposits.htm'
+        soup = self.get_soup(url)
+        content = soup.find_all('table')
+        rates = content[1].find_all('tr')
+
+        boq_td = []
+        for rate in rates:
+            curr = rate.find('td').string
+            if curr is not None:
+                
+                if '3 less than 4' in curr:
+                    short = {
+                        'months': '3',
+                        'rate' : rate.find_all('td')[3].string
+                    }
+                    boq_td.append(short)
+
+                if '6 less than 7' in curr:
+                    mid = {
+                        'months': '6',
+                        'rate' : rate.find_all('td')[3].string
+                    }
+                    boq_td.append(mid)
+                 
+                if '12 less than 24' in curr:
+                    long_ = {
+                        'months': '12',
+                        'rate' : rate.find_all('td')[3].string
+                    }
+                    boq_td.append(long_)
+        
+        return boq_td
+    
+
+    def get_rabo_td(self):
+        url = 'https://www.rabodirect.com.au/rates/personal-rates/'
+        soup = self.get_soup(url)
+        content = soup.find_all('table', {'class': 'rates-table responsive'})
+        rates = content[5].find_all('tr')
+        
+        rabo_td = []
+        for rate in rates:
+            curr = rate.find_all('td')
+            if curr:
+                
+                if '3 months' in curr[0].string:
+                    short = {
+                        'months': '3',
+                        'rate': curr[1].string.strip()
+                    }
+                    rabo_td.append(short)
+                
+                if '6 months' in curr[0].string:
+                    mid = {
+                        'months': '6',
+                        'rate': curr[1].string.strip()
+                    }
+                    rabo_td.append(mid)
+                
+                if '1 year' in curr[0].string:
+                    long_ = {
+                        'months': '12',
+                        'rate': curr[5].string.strip()
+                    }
+                    rabo_td.append(long_)
+        print rabo_td
+        return rabo_td
+
+     
+    def get_bom_td(self):
+        url = 'https://www.bankofmelbourne.com.au/help/interest-rates/fixed-term-deposits'
+        browser = webdriver.PhantomJS(PHANTOMJS_PATH)
+        browser.get(url)
+        soup = bs4(browser.page_source, 'html.parser')
+        rates = soup.find_all('table', {'class': 'table table-striped table-bordered'})
+        content = rates[0].find_all('tr') 
+
+        bom_td = []
+        for rate in content: 
+            curr = rate.find('th').string
+            if curr:
+                
+                if '3 to less than 4 months' in curr:
+                    short = {
+                        'months': '3',
+                        'rate': rate.find('span').string
+                    }
+                    bom_td.append(short)
+                
+                if '6 to less than 7 months' in curr:
+                    mid = {
+                        'months': '6',
+                        'rate': rate.find('span').string
+                    }
+                    bom_td.append(mid)
+
+                if '12 months' == curr:
+                    long_ = {
+                        'months': '12',
+                        'rate': rate.find('span').string
+                    }
+                    bom_td.append(long_)
+        
+        return bom_td
+
+    
+    def get_ing_td(self):
+        url = 'https://www.ingdirect.com.au/savings/term-deposit.html'
+        browser = webdriver.PhantomJS(PHANTOMJS_PATH)
+        browser.get(url)
+        soup = bs4(browser.page_source, 'html.parser')
+        rates = soup.find_all('div', {'class': 'table-row data'})
+
+        ing_td = []
+        for rate in rates:
+            curr = rate.find('p').string
+            
+            if '90 day rate' in curr: 
+                short = {
+                    'months': '3',
+                    'rate': rate.find('span').string
+                }
+                ing_td.append(short)
+
+            if '120 day rate' in curr:
+                short = {
+                    'months': '4',
+                    'rate': rate.find('span').string
+                }
+                ing_td.append(short)
+
+            if '210 day rate' in curr:
+                mid = {
+                    'months': '7',
+                    'rate': rate.find('span').string
+                }
+                ing_td.append(mid)
+
+            if '1 year rate' in curr:
+                long_ = {
+                    'months': '12',
+                    'rate': rate.find('span').string
+                }
+                ing_td.append(long_)
+        
+        note = {
+            'note': 'loyalty bonus +.10',
+            'interest': 'on maturity'
+        }
+        ing_td.append(note)
+        
+        return ing_td
+     
+
+    def get_sun_td(self):
+        url = 'https://www.suncorp.com.au/banking/investments/term-deposits.html'
+        browser = webdriver.PhantomJS(PHANTOMJS_PATH)
+        browser.get(url)
+        soup = bs4(browser.page_source, 'html.parser')
+        rates = soup.find_all('span', {'class': 'rate'})
+    
+        sun_td = [{
+            'months': '3',
+            'rate': rates[0].string
+        },
+        {
+            'months': '6',
+            'rate': rates[1].string
+        },
+        {
+            'months': '12',
+            'rate': rates[2].string
+        }]    
+        
+        return sun_td
+          
+
+    def get_bendigo_td(self):
+        url = 'http://www.bendigobank.com.au/public/personal/term-deposits/term-deposit-interest-rates'         
+        soup = self.get_soup(url)
+        content = soup.find_all('table', {'style': 'width: 100%'})
+        rates = content[1].find_all('tr')
+        
+        bendigo_td = []
+        for rate in rates:
+           
+            curr = rate.find('td')
+            if curr is not None:
+                curr_rate = rate.find_all('td')
+                
+                if '3 Months' == curr.string:
+                    short = {
+                        'months': '3',
+                        'rate': curr_rate[1].string
+                    }
+                    bendigo_td.append(short)
+                
+                if '6 Months' == curr.string:
+                    mid = {
+                        'months': '6',
+                        'rate': curr_rate[1].string
+                    }
+                    bendigo_td.append(mid)
+                
+                if '12 Months' == curr.string:
+                    long_ = {
+                        'months': '12',
+                        'rate': curr_rate[1].string
+                    }
+                    bendigo_td.append(long_)
+
+        return bendigo_td
+    
+    
+    def get_citi_td(self):
+        url = 'https://www.citibank.com.au/term-deposit/?intcid=BN-Productpage-MidpageTD-RET'
+        soup = self.get_soup(url)
+        content = soup.find('div', {'class': 'box blue_box'})
+        rate = str(content.find('h2'))
+        rate = rate.split('>')[1]
+        rate = rate.split('<')[0].split()[0]
+        
+        citi_td = [{
+            'month': '6',
+            'rate': rate
+        }]
+        return citi_td
 
 
     def collate_td(self):
@@ -346,7 +622,14 @@ class WebScrapers(object):
         #nab_td = self.get_nab_td()
         #george_td = self.get_george_td()
         #bankwest_td = self.get_bankwest_td()
-        ubank_td = self.get_ubank_td()
-        
+        #ubank_td = self.get_ubank_td()
+        #boq_td = self.get_boq_td()
+        #rabo_td = self.get_rabo_td()
+        #bom_td = self.get_bom_td()
+        #ing_td = self.get_ing_td()
+        #sun_td = self.get_sun_td() 
+        #bendigo_td = self.get_bendigo_td()
+        citi_td = self.get_citi_td()
+
 
         return None
